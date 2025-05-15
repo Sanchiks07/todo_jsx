4194b4b7fa1ff61b8e509dbb4d22028d107ae75b
ipb23.s.mitniece <ipb23.s.mitniece@pjvt.local>
2025-05-15 11:30:00 +0300

    turpini 3.2

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,32 @@ import DiariesList from './DiariesList.jsx'
 import './App.css'
 
 function App() {
-  const todos = [
+  const todos = useState([
     { id: 1, task: "Iemācīties React", completed: false },
     { id: 2, task: "Iemācīties Laravel", completed: true },
     { id: 3, task: "Nopirkt pienu", completed: false },
-  ];
+  ]);
+
+  newTask = "";
 
   return (
     <>
     <h1>Veicamie uzdevumi</h1>
 
+    <form>
+      <label>
+        <input
+          value={newTask}
+          onChange={(event) => setNewTask(event.target.value)}
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+
     {todos.map((todo) => {
       return <ToDo key={todo.id} {...todo} />;
     })}
-    
+
     <DiariesList/>
     </>
   )
